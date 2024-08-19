399bc871d359fdf2423a902150c0e3056f4c67f9
LovelyCreator <lovely7rh@gmail.com>
2024-08-20 04:55:46 +1000

    update sendgrid

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://95.216.240.153:5000/',
+  baseURL: 'https://booksmart-backend-rcxk.onrender.com',
+  // baseURL : 'https://scarcely-funky-gnu.ngrok-free.app',
+  // baseURL: 'http://95.216.240.153:5000',
   // baseURL: 'http://192.168.142.135:5000',
   headers: {
     'Content-Type': 'application/json'
